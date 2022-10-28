6496decbd397eb711e8e587c7ec38e6d83658421
Alejandro Rafael Alfaro Soto <arasman@gmail.com>
2022-10-28 13:17:51 -0500

    Creation of the components of LoadingQuote and Quote and addition of them into MultipleCustomHook

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,6 +1,5 @@
-import { useFetch } from "../hooks/useFetch";
-import { useCounter} from "../hooks/useCounter";
-import { useEffect } from "react";
+import { useFetch , useCounter} from "../hooks";
+import { LoadingQuote, Quote } from "./";
 
 export const MultipleCustomHooks = () => {
   const {counter, increment} = useCounter(1); 
@@ -20,14 +19,10 @@ export const MultipleCustomHooks = () => {
     <>
       <h1>Breaking Bad Quotes</h1>
       <hr />
-      {isLoading ? (
-        <div className="alert alert-info text-center">Loading...</div>
-      ) : (
-        <blockquote className="blockquote text-end">
-          <p className="mb-1">{quote}</p>
-          <footer className="blockquote-footer">{author}</footer>
-        </blockquote>
-      )}
+      {isLoading 
+        ? <LoadingQuote/>
+       : <Quote quote={quote} author={author}/>
+       }
 
       <button className="btn btn-primary" onClick={() => increment(1)} disabled={isLoading}>
         Next quote
