4627138061604c2bfeea7a1ac93ecf46a5ac6980
Alejandro Rafael Alfaro Soto <arasman@gmail.com>
2022-11-02 16:07:58 -0500

    Routes configuration, addition of usage of Link and NavLink

--- src/09-UseContext/MainApp.jsx
+++ src/09-UseContext/MainApp.jsx
@@ -1,10 +1,20 @@
+import { Navigate, Route, Routes, Link } from "react-router-dom";
+import { HomePage, LoginPage, AboutPage, Navbar } from "./";
 
 
 export const MainApp = () => {
   return (
     <>
-        <h1>Main App</h1>
-        <hr/>
+        <Navbar/>
+        <hr/>        
+        <Routes>
+            <Route path="/" element={ <HomePage/> }/>            
+            <Route path="about" element={ <AboutPage/> }/>
+            <Route path="login" element={ <LoginPage /> }/>
+
+            {/* <Route path="/*" element={ <LoginPage />}/> */}
+            <Route path="/*" element={ <Navigate to="/about"/> }/>
+        </Routes>
     </>
   )
 }
